7c85cef91438b6c686a131c3354596bafddb318a
Serlle Rosale <serlle@caraji.io>
2024-03-26 13:21:21 -0600

    fixed attr description over db

--- db/migrate/20240325220438_create_products.rb
+++ db/migrate/20240325220438_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
       t.string :title
-      t.text :decription
+      t.text :description
       t.string :image_url
       t.decimal :price, precision: 8, scale: 2
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2024_03_25_220438) do
   create_table "products", force: :cascade do |t|
     t.string "title"
-    t.text "decription"
+    t.text "description"
     t.string "image_url"
     t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", null: false
