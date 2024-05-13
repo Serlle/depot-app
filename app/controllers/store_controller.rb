dec875f9938045868692f73f631b9ef29193378e
Serlle Rosales <serlle.rosales96@gmail.com>
2024-05-13 17:00:05 -0600

    new feuture to Cart  - modified partial line item to put the code of cart template  - modified partial cart to get line item partial  - added in application layout render cart to get object cart inside of the sidebar nav  - included CurrentCart concerns inside of store index controller for get cart id in index  - modifie reditec_to in line items controller for redirecting to store index  - created new conditional in application layour for checking object cart and dont have problem with product index

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,14 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_visits, :set_cart
+
   def index
     @products = Product.order(:title)
+  end
 
+  private
+
+  def set_visits
     if session[:counter].nil?
       session[:counter] = 0
     else
