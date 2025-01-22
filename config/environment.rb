33a1cc9531dfb1ac74ab71f6e0a2030c8190bc84
Serlle Rosales <serlle.rosales96@gmail.com>
2025-01-22 10:18:35 -0600

    added system test of background jobs about all process
    - Configured the enverioment test for create ActiveJib correctly.
    - Added system test to validate full checkout flow, including:
      - Payment processing.
      - Email delivery.
      - Order creation validation.

--- config/environment.rb
+++ config/environment.rb
@@ -1,19 +1,21 @@
 # Load the Rails application.
-require_relative "application"
+require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
+  unless Rails.env.test?
+    config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address:        "smtp.gmail.com",
-    port:           587,
-    domain:         "domain.of.sender.net",
-    authentication: "plain",
-    user_name:      "serlle",
-    password:       "secret",
-    enable_starttls_auto: true
-  }
+    config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'domain.of.sender.net',
+      authentication: 'plain',
+      user_name: 'serlle',
+      password: 'secret',
+      enable_starttls_auto: true
+    }
+  end
 end
